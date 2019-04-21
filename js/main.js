6078f44f5181ce7a7984eb07e300e0dfb2c95804
Leo Buchko <31351672+Mir4ri@users.noreply.github.com>
2019-04-21 10:46:06 +0300

    finish desktop

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ jQuery(document).ready(function($) {
     slidesToShow: 2,
     slidesToScroll: 1,
     rows: 2,
-    autoplay: true,
+    autoplay: false,
     autoplaySpeed: 3000,
     arrows: true,
     responsive: [
@@ -83,3 +83,55 @@ function showPanel(panelIndex) {
 }
 
 tabButtons[0].style.backgroundColor = "#FFF";
+
+// to contact modal
+var tcModal = document.getElementById("toContact");
+var tcClose = document.getElementsByClassName("tcCloseMenu-js")[0];
+
+function openToContacts() {
+  tcModal.style.display = "block";
+}
+
+tcClose.onclick = function() {
+  tcModal.style.display = "none";
+};
+
+window.onclick = function(event) {
+  if (event.target === tcModal) {
+    tcModal.style.display = "none";
+  }
+};
+
+// about us modal
+
+var about = document.getElementsByClassName("menu__item")[1];
+var auModal = document.getElementById("aboutUs");
+var auClose = document.getElementsByClassName("auCloseMenu-js")[0];
+
+about.onclick = function() {
+  auModal.style.display = "block";
+};
+
+auClose.onclick = function() {
+  auModal.style.display = "none";
+};
+
+// price modal
+
+var price = document.getElementsByClassName("menu__item")[2];
+var pModal = document.getElementById("price");
+var pClose = document.getElementsByClassName("pCloseMenu-js")[0];
+
+price.onclick = function() {
+  pModal.style.display = "block";
+};
+
+pClose.onclick = function() {
+  pModal.style.display = "none";
+};
+
+window.onclick = function(event) {
+  if (event.target === pModal) {
+    pModal.style.display = "none";
+  }
+};
